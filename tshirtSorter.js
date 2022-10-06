996237d997a23840edfae9d794da9e7de2d17e0d
Greg Stickel <gregory.stickel@live.com>
2022-10-06 09:08:04 -0400

    Update 9:07am

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -1,1 +1,12 @@
 // Write your solution below:
+let string = "slsmmsllsmsmlmsls";
+
+function sorter (str) {
+    //let small = "", medium = "", large = "";
+    let newArr = str.split("");
+    let result = newArr.sort().reverse().join("");
+    return result;
+    
+}
+
+console.log(sorter(string));
